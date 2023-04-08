d8b7fd3272404ff93e7be12474587660621749cc
Ella Robinson <ellaroosmarijn@gmail.com>
2023-04-08 13:45:48 +1200

    feat(server): edit server use routes

--- server/server.ts
+++ server/server.ts
@@ -1,9 +1,17 @@
 import express from 'express'
 import path from 'path'
 
+import routes from './routes/index'
+
 const server = express()
 
 server.use(express.json())
+
 server.use(express.static(path.join(__dirname, 'public')))
+server.use('/api/v1', routes)
+
+server.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'))
+})
 
 export default server
