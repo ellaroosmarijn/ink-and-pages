0f55c3acce645a849958bc60d7de73b68b37b1cf
Ella Robinson <ellaroosmarijn@gmail.com>
2023-04-08 13:48:54 +1200

    feat(client): add front end api
    Add front end api for (1) getting all books, (2) adding a new book, (3) deleting a book, and (4)
    updating a book.

---
+++ client/apis/apiClient.ts
@@ -0,0 +1,47 @@
+import request from 'superagent'
+
+import { Books, Book } from '../../models/books'
+
+export async function allBooks(): Promise<Books> {
+  try {
+    const response = await request.get('/api/v1/books')
+    return response.body
+  } catch (error) {
+    throw Error(
+      'The books disappeared into their worlds of magic and could not attend. Please try again.'
+    )
+  }
+}
+
+export async function newBook(book: Book) {
+  try {
+    const response = await request.post('/api/v1/book/add').send(book)
+    return response.body
+  } catch (error) {
+    throw Error(
+      'The books disappeared into their worlds of magic and could not respond. Please try again.'
+    )
+  }
+}
+
+export async function deleteBook(id: number) {
+  try {
+    const response = await request.delete(`/api/v1/book/${id}`)
+    return response.body
+  } catch (error) {
+    throw Error(
+      'The books disappeared into their worlds of magic and could not respond. Please try again.'
+    )
+  }
+}
+
+export async function updateBook(id: number, book: Book) {
+  try {
+    const response = await request.patch(`/api/v1/book/${id}`).send(book)
+    return response.body
+  } catch (error) {
+    throw Error(
+      'The books disappeared into their worlds of magic and could not attend. Please try again.'
+    )
+  }
+}
