940caa07db73260cbbfb0b5e8fae60cfc1c01e8b
Ella Robinson <ellaroosmarijn@gmail.com>
2023-05-24 13:01:24 +1200

    feat(client): add more syling for header links and add react-anchor-link-smooth-scroll

--- client/components/Header.tsx
+++ client/components/Header.tsx
@@ -1,10 +1,18 @@
 import styled from '@emotion/styled'
+import AnchorLink from 'react-anchor-link-smooth-scroll'
 
-import Books from './Books'
+import { BREAKPOINTS } from '../../shared/constants'
+import Books from './Library'
+
+const HeaderWrapper = styled.div`
+  width: 100%;
+`
 
 const Heading = styled.div`
   font-family: 'Noto Serif JP', serif;
   font-size: 1.5rem;
+  text-transform: uppercase;
+  letter-spacing: 0.5rem;
   text-align: center;
   padding: 0.75rem 0;
   background-color: #ffffff;
@@ -16,20 +24,62 @@ const Divider = styled.div`
 `
 
 const HeadingLinks = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
   background-color: #ffffff;
-  padding: 0.75rem 2rem;
-  display: inline-block;
+  font-family: 'Wix Madefor Display', sans-serif;
+  font-size: 0.65rem;
+  text-transform: uppercase;
+  letter-spacing: 0.1rem;
+  padding: 0.75rem 0;
+  gap: 100px;
+
+  & a:before {
+    text-align: center;
+    display: block;
+  }
+
+  & a {
+    display: inline-block;
+  }
+`
+export const StyledAnchorLink = styled(AnchorLink)`
+  color: inherit;
+  font-weight: 400;
+  text-decoration: none;
+
+  &:hover {
+    color: #8f6917;
+  }
+
+  @media (max-width: ${BREAKPOINTS.md}) {
+    font-weight: 700;
+  }
 `
 
 export default function Header() {
   return (
     <>
-      <Heading>Ink & Pages</Heading>
-      <Divider />
-      <HeadingLinks>Read</HeadingLinks>
-      <HeadingLinks>Reading</HeadingLinks>
-      <HeadingLinks>Collection</HeadingLinks>
-      <Books />
+      <HeaderWrapper>
+        <Heading>Ink & Pages</Heading>
+        <Divider />
+        <HeadingLinks>
+          <StyledAnchorLink href="#library" data-hover="library">
+            Library
+          </StyledAnchorLink>
+          <StyledAnchorLink href="#read" data-hover="read">
+            Read
+          </StyledAnchorLink>
+          <StyledAnchorLink href="#reading" data-hover="reading">
+            Reading
+          </StyledAnchorLink>
+          <StyledAnchorLink href="#rating" data-hover="rating">
+            Rating
+          </StyledAnchorLink>
+        </HeadingLinks>
+        <Books />
+      </HeaderWrapper>
     </>
   )
 }
