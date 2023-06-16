e8ed442209ccdf0cacce0f57369413488d180765
Ella Robinson <ellaroosmarijn@gmail.com>
2023-06-17 10:14:31 +1200

    style(client): change emotion css to tailwind

--- client/components/Header.tsx
+++ client/components/Header.tsx
@@ -1,62 +1,16 @@
-import styled from '@emotion/styled'
 import { Link } from 'react-router-dom'
-
-// import { BREAKPOINTS } from '../../shared/constants'
-
-const HeaderWrapper = styled.div`
-  width: 100%;
-`
-
-const Heading = styled.div`
-  font-family: 'Noto Serif JP', serif;
-  font-size: 1.5rem;
-  text-transform: uppercase;
-  letter-spacing: 0.5rem;
-  text-align: center;
-  padding: 0.75rem 0;
-  background-color: #ffffff;
-`
-
-const Divider = styled.div`
-  background-color: #715312;
-  padding: 0.5px;
-`
-
-const HeadingLinks = styled.div`
-  display: flex;
-  flex-direction: row;
-  justify-content: center;
-  background-color: #ffffff;
-  font-family: 'Wix Madefor Display', sans-serif;
-  font-size: 0.65rem;
-  text-transform: uppercase;
-  letter-spacing: 0.1rem;
-  padding: 0.75rem 0;
-  gap: 100px;
-
-  & a:before {
-    text-align: center;
-    display: block;
-  }
-
-  & a {
-    display: inline-block;
-  }
-`
-
-// @media (max-width: ${BREAKPOINTS.md}) {
-//   font-weight: 700;
-// }
 
 export default function Header() {
   return (
-    <HeaderWrapper>
-      <Heading>Ink & Pages</Heading>
-      <Divider />
-      <HeadingLinks>
+    <div className="w-full">
+      <div className="bg-white py-3 text-center font-title text-2xl uppercase tracking-widest-0.5">
+        Ink & Pages
+      </div>
+      <div className="border-t border-brown-700"></div>
+      <div className="bg-white flex flex-row justify-center gap-x-20 py-3 font-body text-xs uppercase tracking-widest-0.1 hover:text-brown-700 sm:font-semibold md:font-normal">
         <Link to="/library">Library</Link>
         <Link to="/wishlist">Wishlist</Link>
-      </HeadingLinks>
-    </HeaderWrapper>
+      </div>
+    </div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@
 module.exports = {
   content: ['./client/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    colors: {
+      brown: {
+        700: '#715312',
+      },
+    },
+    fontFamily: {
+      title: ['Noto Serif JP', 'serif'],
+      body: ['Wix Madefor Display', 'sans-serif'],
+    },
+    extend: {
+      letterSpacing: {
+        'widest-0.1': '0.1rem',
+        'widest-0.5': '0.5rem',
+      },
+    },
+    plugins: [],
   },
-  plugins: [],
 }
