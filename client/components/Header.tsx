0fe6377dc04cac25550687314ba905eb5de325eb
Ella Robinson <ellaroosmarijn@gmail.com>
2023-06-18 23:16:42 +1200

    style(client): change header size from 2xl to 3xl

--- client/components/Header.tsx
+++ client/components/Header.tsx
@@ -3,11 +3,11 @@ import { Link } from 'react-router-dom'
 export default function Header() {
   return (
     <div className="w-full">
-      <div className="bg-white py-3 text-center font-title text-2xl uppercase tracking-widest-0.5">
+      <div className="bg-white py-3 text-center font-title text-3xl uppercase tracking-widest-0.5">
         Ink & Pages
       </div>
-      <div className="border-t border-brown-700"></div>
-      <div className="bg-white flex flex-row justify-center gap-x-20 py-3 font-body text-xs uppercase tracking-widest-0.1 hover:text-brown-700 sm:font-semibold md:font-normal">
+      <div className="border-brown-700 border-t"></div>
+      <div className="bg-white hover:text-brown-700 flex flex-row justify-center gap-x-20 py-3 font-body text-xs uppercase tracking-widest-0.1 sm:font-semibold md:font-normal">
         <Link to="/library">Library</Link>
         <Link to="/wishlist">Wishlist</Link>
       </div>
